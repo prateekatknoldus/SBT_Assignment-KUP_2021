2d2be560b5a792311b568fde130847f8a3b067c5
prateekatknoldus <prateek.gupta@knoldus.com>
2021-02-23 02:38:26 +0530

    gave proper spacing and indentation

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -1,11 +1,11 @@
-// This class contains methods for performing various operations of a calculator.
+//This class contains methods for performing various operations of a calculator.
 class Calculator {
 
   //This method will return the addition of the given two numbers.
-  def add(firstNum:Int, secondNum:Int):Int = firstNum+secondNum
+  def add(firstNum:Int, secondNum:Int):Int = firstNum + secondNum
 
   //This method will return the subtraction of the given two numbers.
-  def subtract(firstNum:Int, secondNum:Int):Int = firstNum-secondNum
+  def subtract(firstNum:Int, secondNum:Int):Int = firstNum - secondNum
 
   //This method will return the multiplication of the given two numbers.
   def multiply(firstNum:Int, secondNum:Int):Int = firstNum*secondNum
@@ -31,7 +31,7 @@ class Calculator {
     var powerResult = 1
     while(counter<power){
       powerResult = powerResult*number
-      counter=counter+1
+      counter=counter + 1
     }
     powerResult
   }
@@ -45,19 +45,29 @@ class Calculator {
   //This method will return the maximum number from the given two numbers.
   def maxOfTwo(firstNum:Int, secondNum:Int):Int ={
     if(firstNum>secondNum)
-      firstNum
+      {
+        firstNum
+      }
     else
-      secondNum
+      {
+        secondNum
+      }
+
   }
 
   //This method will return the minimum number from the given two numbers.
   def minOfTwo(firstNum:Int, secondNum:Int):Int ={
     if(firstNum<secondNum)
+    {
       firstNum
+    }
     else
+    {
       secondNum
+    }
   }
 
 }
+
 
 
